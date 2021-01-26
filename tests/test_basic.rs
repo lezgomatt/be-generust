88fd0279cc9f3ead8ebc900a724c5d49b6fedf66
Matt <lezgomatt@gmail.com>
2021-01-26 22:29:17 +0800

    tests: Add multi case

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -11,3 +11,15 @@ fn single() -> impl Iterator<Item = i64> {
 fn test_single() {
     assert_eq!(single().collect::<Vec<_>>(), vec![1]);
 }
+
+#[giver]
+fn multi() -> impl Iterator<Item = i64> {
+    give!(1);
+    give!(2);
+    give!(3);
+}
+
+#[test]
+fn test_multi() {
+    assert_eq!(multi().collect::<Vec<_>>(), vec![1, 2, 3]);
+}
